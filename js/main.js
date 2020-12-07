244aaf72ec55d0710895b4ea971e25b7201bc845
e_gunn <gunn816@hotmail.com>
2020-12-07 12:36:45 -0800

    Updated styles

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+/*
+function myFunction() {
+  var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+*/
+
+function showLinks() {
+  var links = document.getElementById("myLinks");
+    if (links.style.display === "block") {
+      links.style.display = "none";
+    } else {
+      links.style.display = "block";
+    }
+}
