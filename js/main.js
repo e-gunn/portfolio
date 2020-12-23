0cb5ca7a1bfc4681c1f083db0ac6b8eb957bf6a0
e_gunn <gunn816@hotmail.com>
2020-12-22 16:37:54 -0800

    add sticky nav

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 
 // change nav display
-
 function showLinks() {
   var links = document.getElementById("myLinks");
     if (links.style.display === "block") {
@@ -10,6 +9,26 @@ function showLinks() {
     }
 }
 
+// add sticky nav
+const nav = document.querySelector("#main");
+let topOfNav = nav.offsetTop;
+
+function fixNav() {
+  if (window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = nav.offsetHeight + "px";
+    document.body.classList.add("fixed-nav");
+  } else {
+    document.body.style.paddingTop = 0;
+    document.body.classList.remove("fixed-nav");
+  }
+}
+
+window.addEventListener("scroll", fixNav);
+
+
+
+
+/*
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {stickNav()};
 
@@ -20,10 +39,11 @@ var navbar = document.getElementById("myLinks");
 var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function addSticky() {  
+function addSticky() { 
   if (window.pageYOffset >= sticky) {   
     navbar.classList.add("sticky") 
   } else {   
     navbar.classList.remove("sticky"); 
   }
 }
+*/
