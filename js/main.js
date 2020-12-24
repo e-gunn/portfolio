60e64292143c6ff4718509b46684da95f007439e
e-gunn <gunn816@hotmail.com>
2020-12-23 17:25:00 -0800

    update hamburger nav

--- js/main.js
+++ js/main.js
@@ -1,14 +1,4 @@
 
-// change nav display
-function showLinks() {
-  var links = document.getElementById("myLinks");
-    if (links.style.display === "block") {
-      links.style.display = "none";
-    } else {
-      links.style.display = "block";
-    }
-}
-
 // add sticky nav
 const nav = document.querySelector("#main");
 let topOfNav = nav.offsetTop;
@@ -25,25 +15,13 @@ function fixNav() {
 
 window.addEventListener("scroll", fixNav);
 
+// close hamburger menu
+function hideMenu(){
+let menuOpen = document.querySelector('#check').checked;
 
-
-
-/*
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {stickNav()};
-
-// Get the navbar
-var navbar = document.getElementById("myLinks");
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function addSticky() { 
-  if (window.pageYOffset >= sticky) {   
-    navbar.classList.add("sticky") 
-  } else {   
-    navbar.classList.remove("sticky"); 
+if(menuOpen = true){
+  document.querySelector('#check').checked = false;
   }
 }
-*/
+
+window.addEventListener("scroll", hideMenu);
